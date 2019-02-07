33f0511b2f4f42551ad13e8f6d546d84c5c042de
NaokoKai <okashimochi@gmail.com>
2019-02-07 12:44:03 +0900

    creat cards

--- ConcentrationGame/main.js
+++ ConcentrationGame/main.js
@@ -1,62 +1,36 @@
-"use strict";
+// "use strict";
 
-const club = [10, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-const diamond = [10, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-const heart = [10, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-const spade = [10, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+const marks = ['club','diamond','heart','spade'];
 
-let clubTurn = Math.floor(Math.random()*club.length);
-
-// console.log(clubTurn);
-// console.log(typeof club[clubTurn]);
-
-let pathList = [
-  'img/card_club_01.png',
-  'img/card_club_02.png',
-  'img/card_club_03.png',
-  'img/card_club_04.png',
-  'img/card_club_05.png',
-  'img/card_club_06.png',
-  'img/card_club_07.png',
-  'img/card_club_08.png',
-  'img/card_club_09.png',
-  'img/card_club_10.png',
-];
-
-console.log(pathList);
-
-// class Cards {
-//   constructor(number, src){
-//     this.number = number;
-//     this.src = src;
-//   }
-// }
-
-// let cardImg = new Array();
-// for (let i=0; i < pathList.length; i++){
-//   console.log(cardImg);
-
-//   let cardImg = new CardImg();
-//   cardImg.src = pathList[i];
-//   img.push(cardImg);
-// }
+let n = Math.floor(Math.random()*marks.length +1);
+console.log(marks[n]);
 
 
+// スタートで開始
 const gameSart = document.getElementById('gameStart');
 gameSart.addEventListener('click', () => {
   gameSart.classList.add('disappearStart');
   for(let i=0; i < 20; i++){
     let card = document.createElement('div');
+    const id = i;
+    card.id = id;
     card.classList.toggle('card');
     document.body.appendChild(card);
-    card.addEventListener('click', () =>{
+
+    // クリックしたらカードが裏返しになる
+    card.addEventListener('click', (e) =>{
+      console.log(e)
+
       card.classList.add('clickedCard');
-      card.style.backgroundColor= "yellow";
-      // card.style.backgroundImage= "url('img/card_club_02.png')";
-      let randomList = Math.floor(Math.random()*pathList.length);
-      console.log(randomList);
-      // card.style.backgroundImage= 'url(pathList[randomList])';
-      card.innerHTMl = pathList[randomList];
+      let num = Math.floor(Math.random() * 10+1);
+      // 一桁のとき頭に0付ける
+      function zeroPadding(num, length) {
+        return ('0' + num).slice(-length);
+      }
+      let cardNum = zeroPadding(num,2);
+      const id = e.srcElement.id;
+      card.id = id;
+      card.style.backgroundImage= `url('img/card_${marks[n]}_${cardNum}.png')`;
     })
   }
 
