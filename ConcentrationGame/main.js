2b17cc78503efdf0350d951d0d757e5ed588a623
NaokoKai <okashimochi@gmail.com>
2019-02-18 17:49:00 +0900

    correct random of marks

--- ConcentrationGame/main.js
+++ ConcentrationGame/main.js
@@ -1,29 +1,30 @@
-// "use strict";
-// トランプマークの種類と
-const marks = ['club','diamond','heart','spade'];
-
-let n = Math.floor(Math.random()*marks.length +1);
-console.log(marks[n]);
-
+"use strict";
 
 // スタートで開始
 const gameSart = document.getElementById('gameStart');
 gameSart.addEventListener('click', () => {
-  gameSart.classList.add('disappearStart');
-  for(let i=0; i < 20; i++){
+gameSart.classList.add('disappearStart');
+  for(let i=0; i < 10; i++){
+    const marks = ['club','diamond','heart','spade'];
+    let n = Math.floor(Math.random()*marks.length);
     let card = document.createElement('div');
     const id = i;
     card.id = id;
+    // カードのクラスcardは裏画面うさぎ
     card.classList.toggle('card');
     document.body.appendChild(card);
 
     // クリックしたらカードが裏返しになる
     card.addEventListener('click', (e) =>{
       console.log(e)
-
+      // トランプマークの配列
+      const marks = ['club','diamond','heart','spade'];
+      // トランプマークをランダムに選択
+      let n = Math.floor(Math.random()*marks.length);
       card.classList.add('clickedCard');
+      // 数字画像が1〜10までしかないのでその中からランダムに数字を選択
       let num = Math.floor(Math.random() * 10+1);
-      // 一桁のとき頭に0付ける
+      // 一桁のとき頭に0付ける（画像の名前に合わせるため）
       function zeroPadding(num, length) {
         return ('0' + num).slice(-length);
       }
@@ -36,3 +37,7 @@ gameSart.addEventListener('click', () => {
 
 
 });
+
+// トランプをクリックするたびマーク・数字が変わるように
+// 同じマークかつ数字が出ないように
+// 同じ数字が揃ったら消えるorカウントする
