076f37644fce5d85ac20c5b76da489272f1ae826
NaokoKai <okashimochi@gmail.com>
2019-02-24 00:37:27 +0900

    define firstCard

--- ConcentrationGame/main.js
+++ ConcentrationGame/main.js
@@ -1,13 +1,16 @@
 "use strict";
 
+
 // カード情報
 const cards = [];
 const marks = ['club','diamond','heart','spade'];
 
 // プレイ中に使用する変数
 let flipCount = 0;
-let firstCard = null;
-let secondCard = null;
+let firstCard;
+let secondCard;
+
+const field = document.getElementById("field");
 
 // スタートで開始
 const gameStart = document.getElementById('gameStart');
@@ -17,8 +20,16 @@ gameStart.addEventListener('click', () => {
   createCards();
 });
 
+function resetFlippedCards() {
+  firstCard = {};
+  secondCard = {};
+  console.log("リセット");
+}
+
 // 引数は必要なし
 function createCards() {
+  resetFlippedCards();
+
   for (let j = 0; j < 4; j++) {
     const mark = marks[j];
 
@@ -30,7 +41,7 @@ function createCards() {
       cards.push(card);
     };
   }
-  // shuffleCards に生成したカードデータを渡す
+  // 生成したカードデータを shuffleCards関数 に渡す
   shuffleCards(); 
 }
 
@@ -42,8 +53,7 @@ function shuffleCards() {
     cards[i] = cards[r];
     cards[r] = tmp;
   }
-  // ゴール：シャッフルしたカードデータを作る
-  console.log(cards);
+  // シャッフルしたカードデータをDOMに書き出すため drawCards関数 に渡す
   drawCards();
 }
 
@@ -52,67 +62,78 @@ function zeroPadding(num, length) {
   return ('0' + num).slice(-length);
 }
 
-// カードデータをもとにDOM操作でカードを表示させる
+// カードデータをもとにDOM操作でカードを表示させる（初期化）
 function drawCards() {
   for (let i = 0; i < cards.length; i++) {
     const card = document.createElement('div');
     // カードのクラスcardは裏画面うさぎ
     card.classList.toggle('card');
-    // クリックしたら裏面が出る
-    card.addEventListener('click', ()=> {
-      let cardNum = zeroPadding(cards[i].num, 2);
-      card.style.backgroundImage= `url('img/card_${cards[i].mark}_${cardNum}.png')`;
-      console.log(card);
-    // card.classList.toggle('clickedCard');
-    });
-    document.body.appendChild(card);
+    card.id = i;
+    field.appendChild(card);
   }
 }
 
-// for(let i = 0; i < 10; i++) {
+// DOMでクリックした場所の情報を取得してflipcardに渡す
+field.addEventListener('click', (e)=> {
+  const cardId = e.srcElement.id;
+  flipCard(cardId);
+});
 
-//   let count = 0;
-//   // クリックしたらカードが裏返しになる
-//   card.addEventListener('click', e => {
-//     flipCard(e);
-//     count++;
-//     if (count > 3) {
-//       return;
-//       // card.classList.add('card');
-//     } else {
-//       const marks = ['club','diamond','heart','spade'];
-//       // トランプマークをランダムに選択
-//       let n = Math.floor(Math.random() * marks.length);
-//       card.classList.add('clickedCard');
-//       // 数字画像が1〜10までしかないのでその中からランダムに数字を選択
-//       let num = Math.floor(Math.random() * 10+1);
-//       // 一桁のとき頭に0付ける（画像の名前に合わせるため）
-//       function zeroPadding(num, length) {
-//         return ('0' + num).slice(-length);
-//       }
-//       let cardNum = zeroPadding(num, 2);
-//       const id = e.srcElement.id;
-//       card.id = id;
-//       card.style.backgroundImage= `url('img/card_${marks[n]}_${cardNum}.png')`;
-//     }
+// クリックされたカードを開けて、DOMに描く
+function flipCard(cardIndex) {
+  const card = cards[cardIndex];
 
-//   });
-// };
+  // 配列cards内でindex番号"cardNum番目"のopen情報をtrueに変更
+  card.open = true;
 
-function flipCard(card) {
-  console.log('card', card);
-  if (firstCard !== null && secondCard !== null) {
-    return;
-  }
-  card.className = 'cardOpen';
+  // HTML内でidが"cardIndex"のものを取得
+  const cardDom = document.getElementById(cardIndex);
+
+  // 画像名に合わせるために番号を取得
+  const formattedNum = zeroPadding(card.num, 2);
+  
+  // 先程取得した"card"のstyleを変更する
+  cardDom.style.backgroundImage= `url('img/card_${card.mark}_${formattedNum}.png')`;
+
+  checkFlipLimit(cardIndex);
+}
+
+// １枚めのカードと２枚めのカードを照合
+// 同じ番号の場合、fistCard, secondCardを初期化するのみ。
+// 違う番号の場合、１秒後に裏に戻す。その間、他のカードはクリックできない。
+// firstCard, secondCardを初期化。
+function checkFlipLimit(cardIndex) {
+  const card = cards[cardIndex];
+
+  // めくった回数をカウント(0 〜 2まで。2になったら0に戻す)
   flipCount++;
-  if(flipCount % 2 === 1) {
-    firstCard = card.srcElement.id;
-  } else {
-    secondCard = card.srcElement.id;
+  // めくった回数によって1枚目、2枚目のカード番号を保持
+  if (flipCount === 1) {
+    firstCard.num = card.num;
+    firstCard.index = cardIndex;
   }
-}
 
+  if (flipCount === 2) {
+    secondCard.num = card.num;
+    secondCard.index = cardIndex;
+
+    // 1枚目と2枚目が同じ番号の場合、fistCard, secondCardを初期化するのみ。
+    // カードは見えたまま
+    if (firstCard.num === secondCard.num) {
+      resetFlippedCards();
+    }
+    // 違う番号の場合、１秒後に裏に戻す。
+    // その間、他のカードはクリックできない。
+    if (firstCard.num !== secondCard.num) {
+
+      // const cardDom = 
+      resetFlippedCards();
+    }
+
+    flipCount = 0;
+  }
+
+}
 
 // トランプをクリックするたびマーク・数字が変わるように
 // 同じマークかつ数字が出ないように
